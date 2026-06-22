c364632c94bfbc8c9fd77778a227c122c4fc5937
Mayank Singh <54627924+ms1835@users.noreply.github.com>
2026-06-22 23:31:24 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
+            return a.start < b.start;
+        });
+        priority_queue<int, vector<int>, greater<int> > pq;
+        for(const auto &interval: intervals){
+            if(!pq.empty() && pq.top() <= interval.start)
+                pq.pop();
+            pq.push(interval.end);
+        }
+        return pq.size();
+    }
+};
